f52a1145996abb0bce9e54be91c7be151b79020f
Chokan <chokan.dzhunusov@gmail.com>
2022-08-10 09:25:34 +0300

    ADD: problem of generating document with given chars

---
+++ Strings/generate_document.py
@@ -0,0 +1,28 @@
+"""
+Time complexity: O(n)
+Space complexity: O(n)
+"""
+
+
+def generate_document(characters: 'str', document: 'str') -> 'bool':
+    characters_count = {}
+
+    for char in characters:
+        if char not in characters_count:
+            characters_count[char] = 0
+        characters_count[char] += 1
+    
+    for char in document:
+        if char not in characters_count or characters_count[char] == 0:
+            return False
+        characters_count[char] -= 1
+    return True
+        
+
+def main() -> None:
+    ans = generate_document('Bste!hetsi  ', 'is the Best!')
+    print(f'The answer is: {ans}')
+
+
+if __name__ == '__main__':
+    main()
